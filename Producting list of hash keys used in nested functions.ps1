87fdf31e1c1d8eb5f94fdbd6540f3a304d2c8522
kaeaddaar <playinfc@hotmail.ca>
2017-03-01 09:33:30 -0800

    nested function test

---
+++ Producting list of hash keys used in nested functions.ps1
@@ -0,0 +1,13 @@
+﻿$Arr = New-Object System.Collections.ArrayList
+
+function AA 
+{
+    $X = foreach ($item in @("One", "Two", "Three")) {$Arr.add($item)}
+    function BB
+    {
+        @("Subnet.Name", "Subnet", "Subnet.AddressPrefix")
+    }
+    $X = foreach ($item in BB) {$Arr.Add($item)}
+}    
+AA
+
