95824fb8b0f369a4baf22a43fddbf8a58d02fabc
kaeaddaar <playinfc@hotmail.ca>
2017-02-28 23:55:44 -0800

    Working on automation,  secrets, and json templates

---
+++ How to get a password from an automation variable.ps1
@@ -0,0 +1,18 @@
+﻿Login-AzureRmAccount -SubscriptionName "VSE MPN"
+
+get-command *variable*
+Get-AzureRmResourceGroup | Select-Object -Property ResourceGroupName
+Get-AzureRmAutomationVariable -ResourceGroupName "DevAPITest6" -AutomationAccountName "DevAPIAutomation"
+
+$PWD = Get-AzureRmAutomationVariable -Name "PWD" -ResourceGroupName "DevAPITest6" -AutomationAccountName "DevAPIAutomation"
+
+get-command *secret*
+
+# how to get from a secret in a key vault
+$PWD = Get-AzureKeyVaultSecret -VaultName "DevAPI6KeyVault" -Name "WWS5PWD"
+$PWD.SecretValueText #Clear Test PWD
+
+get-command *Secure*
+
+$PWD
+
---
+++ How to get content from JSON Files.ps1
@@ -0,0 +1,10 @@
+﻿
+get-command *json*
+Get-Command *content*
+
+Get-AzureRmAutomationVariable -Name ""PWD"" -ResourceGroupName ""DevAPITest6"" -AutomationAccountName ""DevAPIAutomation""
+
+
+$X = (Get-Content -Path .\LiveInstance.json) -join "`n" | ConvertFrom-Json
+
+$X.parameters
