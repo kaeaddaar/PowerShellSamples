d91252b210f3bf4d453eaa7f7d65b935578cf517
kaeaddaar <playinfc@hotmail.ca>
2017-03-08 00:30:04 -0800

    Add sample code for copy VHD
    This is to make a copy of the Sysprepped VHD

---
+++ copy vhd.ps1
@@ -0,0 +1,12 @@
+﻿
+$srcUri = "https://cmsvcstorageacct.blob.core.windows.net/vhds/cmSvcOsDisk1.vhd"
+$storageAccount = "cmsvcstorageacct"
+$storageKey = "VPoGg7pIpIKYnMqZOa2vSxyKWwlACBmJqHNPYsvzCrAqQ1GLTKjmiUBzSJiCiE4DlgrUwqD6/kkrycG9zS7tMA=="
+
+$destContext = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $storageKey
+
+$containerName = "syspreppedvhds"
+
+New-AzureStorageContainer -Name $containerName -Context $destContext
+
+$blob1 = Start-AzureStorageBlobCopy -srcUri $srcUri -DestContainer $containerName -DestBlob "cmSvcOsDisk_SysPrepped.vhd" -DestContext $destContext
