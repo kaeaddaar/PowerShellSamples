89aad98fbf65bc7b0aa8081a684729621fcb512e
kaeaddaar <playinfc@hotmail.ca>
2017-06-16 17:15:52 -0700

    check wws5 registry and find associated service part 1

---
+++ How To Get WWS5 data from registry, find service, and check to see if it is running.ps1
@@ -0,0 +1,17 @@
+﻿Get-Service | Where-Object {$_.Name -like "SystemFiveDeptDemoETLService"} | Select-Object -Property "Name"
+
+$Temp = Get-ChildItem -path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Windward\System Five\'
+
+[Int32]$i = 0
+
+$DataSets = $Temp | Select-Object -Property "PSChildName","Name"
+foreach ($D in $DataSets)
+{
+    " " + $i + " " + $D.PSChildName + " === `"" + $Temp[$i].SubKeyCount + "`""
+    $i = $i + 1
+}
+
+$Choice = Read-Host "Enter a choice"
+$Temp[$Choice]
+$X = get-itemproperty -Path ("Registry::" + $DeptDemo.Name)
+$X.'(default)'
