a26d30e5000955777ced4d6fda99680af7db8f68
kaeaddaar <playinfc@hotmail.ca>
2017-06-29 12:37:33 -0700

    Working POC for database backup, time to use this as an excercise for TDD

--- TimeTracking.ps1
+++ TimeTracking.ps1
@@ -35,20 +35,26 @@ function ExecuteSqlQuery ($Server, $Database, $SQLQuery) {
 $resultsDataTable = New-Object System.Data.DataTable
 $resultsDataTable = ExecuteSqlQuery $Server $Database $UserSqlQuery $UserPSqlQuery
 
-
-#validate we got data
-Write-Host ("The table contains: " + $resultsDataTable.Rows.Count + " rows")
-
-function Task ([switch]$Start, [string]$Task50)
+function Set-Task ([string]$Task50,[switch]$Start) # start or end a task
 {
+    [string]$StartEndDateTime = get-date -Format "yyyy-MM-dd HH:mm:ss"
     if ($Start) { $TrueOrFalse = 1 } Else {$TrueOrFalse = 0}
-    $sql = ("INSERT INTO [dbo].[TimeTracking_simple] (Task, bStarting) VALUES ('" + $Task50 + "', " + $TrueOrFalse + " );")
+    $sql = ("INSERT INTO [dbo].[TimeTracking_simple] (Task, bStarting, StartEndDateTime) VALUES ('" + $Task50 + "', '" + $TrueOrFalse + "', '" + $StartEndDateTime + "' );")
     ExecuteSqlQuery $Server $Database $sql
 }
 
-Task "TDD" -Start
-if ($false)
+if ($started -ne $true)
 {
-    # Stops Task
-    Task "Remoting"
+    $msg = Read-Host -Prompt "Enter a description for your task up to 50 characters."
+    # trim to 50
+    if ($msg.Length -gt 50) { Write-Output ("" + $msg.Length + " records is too long truncated to 50 characters: "); $msg = $msg.trimstart(50); Write-Output ("`'" + $msg + "`"") }
+    # when blank set default
+    if ($msg.Length -eq 0) { Write-Output ("nothing entered, using `"(default)`""); $msg = "(default)" }
 }
+
+if ($started = $true) # end task
+{ Set-Task -Task50 $msg; $started = $false }
+else                  # start0 Task
+{ Set-Task -Task50 $msg -Start; $started = $true }
+
+# how long has it been since task started/ended?
